c8701a8391548affb1aa03b12f3549b8e189d427
alexdum <alexandru.dumitrescu@gmail.com>
2019-08-22 21:49:25 +0300

    plot seasons

--- plot_multianual_static.R
+++ plot_multianual_static.R
@@ -1,4 +1,12 @@
 library(raster)
+library(rnaturalearth)
+library(classInt)
+library(RColorBrewer)
+
+# prepare the layer with world borders
+countries <- ne_countries(scale = 110, type = "countries")
+countries.line <- as(countries, "SpatialLines")
+countries.sp <- list("sp.lines", countries.line, col = "#525252",lwd = 1)
 
 # load seasonal data
 r.seas <- brick("nc/ghi_seasons_1981_2018.nc")
@@ -16,6 +24,21 @@ for (i in season) {
 }
 
 names(r.multi) <- c("DJF","MAM", "JJA", "SON")
+r.multi[] <- round(getValues(r.multi), 1)
 
-spplot(r.multi)
+# # check range of values
+# summary(brick(r.multi))
+# find the preaks with pretty
+brks <- classIntervals(as.vector(getValues(r.multi)), n = 9, style = "pretty")
+cols <- colorRampPalette(brewer.pal(length(brks$brks),"YlOrRd"))
 
+
+p1 <- spplot(r.multi, sp.layout = list(countries.sp), scales = list(draw = TRUE, cex = 0.8),
+             col.regions = cols,cuts = length(brks$brks) - 1,at = brks$brks,
+             colorkey = list(height = 0.8,
+                             labels = list(
+                               at = brks$brks[2:(length(brks$brks) - 1)],
+                               labels = round(brks$brks,2)[2:(length(brks$brks) - 1)]
+                             ), space = "bottom"),
+             par.settings = list(strip.background = list(col = "white")))
+plot(p1)
